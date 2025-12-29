bd9f66bfc47bc8436799fcbf39e7e8fabba8d67f
srhiulli <srhiulli@gmail.com>
2025-12-29 17:16:15 -0300

    feat: use state Pattern

---
+++ GameState.js
@@ -0,0 +1,188 @@
+export class GameState {
+    constructor(gameConfig) {
+        this.gameConfig = gameConfig; 
+        this.currentState = new ReadyState(this);
+        this.score = 0;
+        this.pacman = null;
+        this.ghosts = [];
+        this.timer = null;
+        this.powerPillTimer = null;
+    }
+    
+    setState(state) {
+        this.currentState = state;
+    }
+    
+    start() { this.currentState.start(); }
+    pause() { this.currentState.pause(); }
+    gameOver(win) { this.currentState.gameOver(win); }
+    update() { this.currentState.update(); }
+    
+    resetGame() {
+        this.score = 0;
+        this.pacman = null;
+        this.ghosts = [];
+        if (this.timer) clearInterval(this.timer);
+        if (this.powerPillTimer) clearTimeout(this.powerPillTimer);
+    }
+}
+
+class ReadyState {
+    constructor(gameState) {
+        this.gameState = gameState;
+    }
+    
+    start() {
+        const gs = this.gameState;
+        const config = gs.gameConfig;
+        
+        gs.resetGame();
+        config.startButton.classList.add('hide');
+        config.gameBoard.createGrid(config.LEVEL);
+        
+        gs.pacman = new config.Pacman(2, 287);
+        config.gameBoard.addObject(287, [config.OBJECT_TYPE.PACMAN]);
+        
+        const handleInput = (e) => 
+            gs.pacman.handleKeyInput(e, config.gameBoard.objectExist.bind(config.gameBoard));
+        document.addEventListener('keydown', handleInput);
+        
+        // Criar Ghosts
+        gs.ghosts = [
+            new config.Ghost(5, 188, config.blinkyBehavior, config.OBJECT_TYPE.BLINKY),
+            new config.Ghost(4, 209, config.pinkyBehavior, config.OBJECT_TYPE.PINKY),
+            new config.Ghost(3, 230, config.inkyBehavior, config.OBJECT_TYPE.INKY),
+            new config.Ghost(2, 251, config.clydeBehavior, config.OBJECT_TYPE.CLYDE)
+        ];
+        
+        gs.timer = setInterval(() => this.gameState.update(), config.GLOBAL_SPEED);
+        
+        this.gameState.setState(new PlayingState(this.gameState));
+    }
+    
+    pause() { console.log('Cannot pause - game not started'); }
+    gameOver() { console.log('Cannot game over - game not started'); }
+    update() { }
+}
+
+class PlayingState {
+    constructor(gameState) {
+        this.gameState = gameState;
+    }
+    
+    start() { console.log('Already playing'); }
+    
+    pause() {
+        clearInterval(this.gameState.timer);
+        this.gameState.setState(new PausedState(this.gameState));
+    }
+    
+    gameOver(win) {
+        const gs = this.gameState;
+        const config = gs.gameConfig;
+        
+        clearInterval(gs.timer);
+        clearTimeout(gs.powerPillTimer);
+        
+        config.gameBoard.removeObject(gs.pacman.pos, [config.OBJECT_TYPE.PACMAN]);
+        config.gameBoard.showGameStatus(win);
+        config.startButton.classList.remove('hide');
+        
+        this.gameState.setState(new GameOverState(this.gameState, win));
+    }
+    
+    update() {
+        const gs = this.gameState;
+        const config = gs.gameConfig;
+        
+        config.gameContext.updateContext(gs.ghosts);
+        config.gameBoard.moveCharacter(gs.pacman);
+        this.checkCollision();
+        
+        gs.ghosts.forEach(ghost => {
+            config.gameBoard.moveGhost(ghost, gs.pacman, config.gameContext);
+        });
+        this.checkCollision();
+        
+        if (config.gameBoard.objectExist(gs.pacman.pos, config.OBJECT_TYPE.DOT)) {
+            config.gameBoard.removeObject(gs.pacman.pos, [config.OBJECT_TYPE.DOT]);
+            config.gameBoard.dotCount--;
+            gs.score += 10;
+        }
+        
+        if (config.gameBoard.objectExist(gs.pacman.pos, config.OBJECT_TYPE.PILL)) {
+            config.gameBoard.removeObject(gs.pacman.pos, [config.OBJECT_TYPE.PILL]);
+            gs.pacman.powerPill = true;
+            gs.score += 50;
+            
+            clearTimeout(gs.powerPillTimer);
+            gs.powerPillTimer = setTimeout(
+                () => (gs.pacman.powerPill = false),
+                config.POWER_PILL_TIME
+            );
+        }
+        
+        gs.ghosts.forEach((ghost) => (ghost.isScared = gs.pacman.powerPill));
+        
+        if (config.gameBoard.dotCount === 0) {
+            this.gameOver(true);
+        }
+        
+        config.scoreTable.innerHTML = gs.score;
+    }
+    
+    checkCollision() {
+        const gs = this.gameState;
+        const config = gs.gameConfig;
+        const collidedGhost = gs.ghosts.find((ghost) => gs.pacman.pos === ghost.pos);
+        
+        if (collidedGhost) {
+            if (gs.pacman.powerPill) {
+                config.gameBoard.removeObject(collidedGhost.pos, [
+                    config.OBJECT_TYPE.GHOST,
+                    config.OBJECT_TYPE.SCARED,
+                    collidedGhost.name
+                ]);
+                collidedGhost.pos = collidedGhost.startPos;
+                gs.score += 100;
+            } else {
+                this.gameOver(false);
+            }
+        }
+    }
+}
+
+class PausedState {
+    constructor(gameState) {
+        this.gameState = gameState;
+    }
+    
+    start() {
+        const config = this.gameState.gameConfig;
+        this.gameState.timer = setInterval(
+            () => this.gameState.update(), 
+            config.GLOBAL_SPEED
+        );
+        this.gameState.setState(new PlayingState(this.gameState));
+    }
+    
+    pause() { console.log('Already paused'); }
+    gameOver() { console.log('Cannot game over while paused'); }
+    update() { }
+}
+
+class GameOverState {
+    constructor(gameState, win) {
+        this.gameState = gameState;
+        this.win = win;
+    }
+    
+    start() {
+        this.gameState.setState(new ReadyState(this.gameState));
+        this.gameState.start();
+    }
+    
+    pause() { console.log('Cannot pause - game is over'); }
+    gameOver() { console.log('Already game over'); }
+    update() { }
+}
--- index.js
+++ index.js
@@ -1,123 +1,36 @@
 import { LEVEL, OBJECT_TYPE } from './setup';
-import  {GameBoard , GameContext} from './GameBoard';
+import { GameBoard, GameContext } from './GameBoard';
 import Pacman from './Pacman';
 import Ghost from './Ghosts';
 import { blinkyBehavior, pinkyBehavior, inkyBehavior, clydeBehavior } from './ghostBehaviors';
-
-
+import { GameState } from './GameState';
 
 const gameGrid = document.querySelector('#game');
 const scoreTable = document.querySelector('#score');
 const startButton = document.querySelector('#start-button');
-const POWER_PILL_TIME = 10000; // ms
-const GLOBAL_SPEED = 80; // ms
+
 const gameBoard = GameBoard.createGameBoard(gameGrid, LEVEL);
 const gameContext = GameContext.createGameContext(gameBoard);
-let score = 0;
-let timer = null;
-let gameWin = false;
-let powerPillActive = false;
-let powerPillTimer = null;
 
+const gameState = new GameState({
+    gameBoard,
+    gameContext,
+    startButton,
+    scoreTable,
+    LEVEL,
+    OBJECT_TYPE,
+    Pacman,
+    Ghost,
+    blinkyBehavior,
+    pinkyBehavior,
+    inkyBehavior,
+    clydeBehavior,
+    POWER_PILL_TIME: 10000,
+    GLOBAL_SPEED: 80
+});
 
+startButton.addEventListener('click', () => gameState.start());
 
-function gameOver(pacman, grid) {
-
-  document.removeEventListener('keydown', (e) =>
-    pacman.handleKeyInput(e, gameBoard.objectExist.bind(gameBoard))
-  );
-
-  gameBoard.showGameStatus(gameWin);
-
-  clearInterval(timer);
-  startButton.classList.remove('hide');
-}
-
-function checkCollision(pacman, ghosts) {
-  const collidedGhost = ghosts.find((ghost) => pacman.pos === ghost.pos);
-
-  if (collidedGhost) {
-    if (pacman.powerPill) {
-      gameBoard.removeObject(collidedGhost.pos, [
-        OBJECT_TYPE.GHOST,
-        OBJECT_TYPE.SCARED,
-        collidedGhost.name
-      ]);
-      collidedGhost.pos = collidedGhost.startPos;
-      score += 100;
-    } else {
-      gameBoard.removeObject(pacman.pos, [OBJECT_TYPE.PACMAN]);
-      gameBoard.rotateDiv(pacman.pos, 0);
-      gameOver(pacman, gameGrid);
-    }
-  }
-}
-
-function gameLoop(pacman, ghosts) {
-   gameContext.updateContext(ghosts);
-
-  gameBoard.moveCharacter(pacman);
-  checkCollision(pacman, ghosts);
-
-  ghosts.forEach(ghost => {
-    gameBoard.moveGhost(ghost, pacman, gameContext);
-  });
-  checkCollision(pacman, ghosts);
-
-  if (gameBoard.objectExist(pacman.pos, OBJECT_TYPE.DOT)) {
-
-    gameBoard.removeObject(pacman.pos, [OBJECT_TYPE.DOT]);
-    gameBoard.dotCount--;
-    score += 10;
-  }
-  if (gameBoard.objectExist(pacman.pos, OBJECT_TYPE.PILL)) {
-
-    gameBoard.removeObject(pacman.pos, [OBJECT_TYPE.PILL]);
-
-    pacman.powerPill = true;
-    score += 50;
-
-    clearTimeout(powerPillTimer);
-    powerPillTimer = setTimeout(
-      () => (pacman.powerPill = false),
-      POWER_PILL_TIME
-    );
-  }
-  if (pacman.powerPill !== powerPillActive) {
-    powerPillActive = pacman.powerPill;
-    ghosts.forEach((ghost) => (ghost.isScared = pacman.powerPill));
-  }
-  if (gameBoard.dotCount === 0) {
-    gameWin = true;
-    gameOver(pacman, gameGrid);
-  }
-  scoreTable.innerHTML = score;
-}
-
-function startGame() {
-
-  gameWin = false;
-  powerPillActive = false;
-  score = 0;
-
-  startButton.classList.add('hide');
-
-  gameBoard.createGrid(LEVEL);
-
-  const pacman = new Pacman(2, 287);
-  gameBoard.addObject(287, [OBJECT_TYPE.PACMAN]);
-  document.addEventListener('keydown', (e) =>
-    pacman.handleKeyInput(e, gameBoard.objectExist.bind(gameBoard))
-  );
-
-const ghosts = [
-  new Ghost(5, 188, blinkyBehavior, OBJECT_TYPE.BLINKY),
-  new Ghost(4, 209, pinkyBehavior, OBJECT_TYPE.PINKY),
-  new Ghost(3, 230, inkyBehavior, OBJECT_TYPE.INKY),
-  new Ghost(2, 251, clydeBehavior, OBJECT_TYPE.CLYDE)
-];
-
-  timer = setInterval(() => gameLoop(pacman, ghosts), GLOBAL_SPEED);
-}
-
-startButton.addEventListener('click', startGame);
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') gameState.pause();
+});
